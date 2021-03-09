3db90b200c9e814b1b650b8b7fb798a890f4763f
tothmond <raytoth626@gmail.com>
2021-03-09 01:51:44 +0000

    Completed Success on all tests!

--- main.fs
+++ main.fs
@@ -3,19 +3,39 @@ module Assignment
 // Problem 1
 let rec prod (lst:int list) =
     // write your solution here
-    0
+  if List.isEmpty lst
+  then 1
+  else List.head lst * prod (List.tail lst)
+
 
 // Problem 2
 let rec map f (lst:int list) =
     // write your solution here
-    []
-
+  
+  if List.isEmpty lst
+  then [] 
+  else f (List.head lst) :: map f (List.tail lst)
+       
 // Problem 3
 let rec odd (lst:int list) =
     // write your solution here
-    lst
+    if List.isEmpty lst
+    then lst
+    else if List.head lst % 2 <> 0
+    then List.head lst :: odd (List.tail lst)
+    else odd (List.tail lst)
+
+    
+
+    
+    
+    
 
 // Problem 4
 let rec filter f lst =
     // write your solution here
-    lst
+    if List.isEmpty lst
+    then lst
+    else if f(List.head lst)
+    then List.head lst :: filter f (List.tail lst)
+    else filter f (List.tail lst)
